eca6bfe1ca6fd9ef6e363626b66ad72bfd97fe3f
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2025-01-04 14:54:42 +0200

    device commit

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 20 }
+
+  validate :validate_name
+
+  validates :role, presence: true
+
+  enum :role, [ :student, :instructor, :admin ], validate: true
+
+  def validate_name
+    errors.add(:name, "does not match Regex") unless name.strip =~ /\A([a-z]{3,})(\s+[a-z]{3,})*\Z/i
+  end
+
+end
